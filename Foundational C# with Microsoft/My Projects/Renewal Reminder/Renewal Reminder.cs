ed16cfe53c9a1f74bd797d3db2c3d86d31eb5761
Sevinda-Herath <119860637+Sevinda-Herath@users.noreply.github.com>
2023-09-26 21:36:46 +0530

    Add files via upload

---
+++ Foundational C# with Microsoft/My Projects/Renewal Reminder/Renewal Reminder.cs
@@ -0,0 +1,30 @@
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+// Your code goes here
+
+if (daysUntilExpiration <= 5 && daysUntilExpiration > 1)
+{
+    discountPercentage += 10;
+    Console.WriteLine(@$"Your subscription expires in {daysUntilExpiration} days.
+Renew now and save {discountPercentage}%!");
+}
+if (daysUntilExpiration <= 10 && daysUntilExpiration > 5)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+if (daysUntilExpiration == 1)
+{
+    discountPercentage += 20;
+    Console.WriteLine(@$"Your subscription expires in {daysUntilExpiration} days.
+Renew now and save {discountPercentage}%!");
+}
+if (daysUntilExpiration == 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+else
+{
+    
+}
